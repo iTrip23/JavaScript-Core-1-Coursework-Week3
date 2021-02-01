c2bee33a2cd10ca568f2b3bd7af5a2bd2dd3f89d
Ionut Tripa <52714188+iTrip23@users.noreply.github.com>
2021-02-01 20:57:07 +0000

    Finished homework

--- 3-extra/1-card-validator.js
+++ 3-extra/1-card-validator.js
@@ -26,29 +26,30 @@ a92332119c011112 (invalid characters)
 ```
 
 These are the requirements your project needs to fulfill:
-
 - Make a JavaScript file with a name that describes its contents.
 - Create a function with a descriptive name which makes it clear what the function does. The function should take one argument, the credit card number to validate.
 - Write at least 2 comments that explain to others what a line of code is meant to do.
 - Return a boolean from the function to indicate whether the credit card number is valid.
 - Use `node` from the command line to test if your code works as expected.
-
 Good luck!
 */
-let cardNum = 123456234;
 
-function cardValidator (num) {
-    let strNum = (num +'').split('');//turns the number in an array of strings
+function cardValidator (cardNum) {
+    if(typeof cardNum !== 'number') return false;//checks if the input is a number
+    let strNum = ('' + cardNum).split('');//turns the number in an array of strings
+    if(strNum.length !== 16) return false;//checks the length of the string
     let arrNum = [];
-    for(let i = 0; i < strNum.length; i++) {
-        if(typeof +strNum[i] === 'number') strNum.push(strNum[i]);
-        else return 'Not a valid credit card number';
+    for(let i = 0; i < strNum.length; i++) { //checks if all the values are numbers;
+        if(typeof +strNum[i] === 'number') {
+            arrNum.push(+strNum[i]);
+        }
+        else return false;
     };
-    if(strNum.length !== 16) return 'Not a valid credit card number';//checks the length of the string
-    if(strNum[strNum.length-1] % 2 !== 0) return 'Not a valid credit card number';//checks if the last number is even
-    if(Math.min(arrNum) === Math.max(arrNum)) return 'Not a valid credit card number';//checks if at least 2 numbers present
-    console.log(Math.min(+strNum[strNum.length-1]));
-    console.log(Math.max(arrNum));
+    if(strNum.length !== 16) return false;//checks the length of the array to be 16
+    if(strNum[strNum.length-1] % 2 !== 0) return false;//checks if the last number is even
+    if(Math.min(...arrNum) === Math.max(...arrNum)) return false;//checks if at least 2 numbers present
+    if((arrNum.reduce((a, b) => a + b)) <= 16) return false;//checks if the sum of all the numbers is greater than 16
+    return true;
 }
 
-console.log(cardValidator(cardNum));
+console.log(cardValidator(9999777788880000));
